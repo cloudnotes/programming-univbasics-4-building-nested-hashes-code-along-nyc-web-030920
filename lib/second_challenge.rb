8ef15cef7b90f74b923319f24a1045f9324cc24d
Zhiyuan Liu <cloudnotes.64@gmail.com>
2020-03-04 14:49:04 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,12 +2,21 @@ def second_challenge
   epic_tragedy = {
     :montague => 
       {
-        :patriarch
+        :patriarch => {},
+        :matriarch => {},
+        :hero => {},
+        :hero_friends => []
       }, 
+      
     :capulet => 
       {
-     
-      }}
+        :patriarch => {},
+        :matriarch => {},
+        :heroine => ,
+        :heroine_friends => []
+      }
+    
+  }
 
   
 end
