a496f7c16cf661dfc0bcc1d29b52f430677f7323
Zhiyuan Liu <cloudnotes.64@gmail.com>
2020-03-04 15:17:51 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,8 +6,15 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => 
       [
-        {:name =>},
-        {}
+        {:name =>,
+         :age =>,
+         :attitude =>
+        },
+        {
+         :name =>,
+         :age =>,
+         :attitude =>
+        }
       ]
    }, 
    :capulet => {
